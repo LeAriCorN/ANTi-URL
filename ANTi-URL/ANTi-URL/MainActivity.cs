21444d3eab8c507191e20c04876d4fb2dcd2e02a
권예원 <pandorasb0x@naver.com>
2017-05-22 09:56:50 +0900

    시작합니다. 마스터 첫부분
    오예 시작이닷

---
+++ ANTi-URL/ANTi-URL/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ANTi_URL
+{
+    [Activity(Label = "ANTi_URL", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+             SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
