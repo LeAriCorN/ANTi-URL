ffabf74791b8a94b381806f87a878bc008b1be78
권예원 <pandorasb0x@naver.com>
2017-05-22 11:06:01 +0900

    url 탐지까지 완성

--- ANTi-URL/ANTi-URL/MainActivity.cs
+++ ANTi-URL/ANTi-URL/MainActivity.cs
@@ -12,8 +12,40 @@ namespace ANTi_URL
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-             SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.gotit);
+
+            button.Click += changedownlabel;
+
+        }
+
+        private void changedownlabel(object sender, System.EventArgs e)
+        {
+            EditText upeditor = FindViewById<EditText>(Resource.Id.editor);
+            TextView showlabel = FindViewById<TextView>(Resource.Id.changelabel);
+
+            string res = upeditor.Text;
+
+            if (chkurl(res) == true)
+            {
+                showlabel.Text = res.ToString();
+            }
+            else
+            {
+                Toast.MakeText(this, "URL 형식에 맞지 않습니다", ToastLength.Long).Show();
+            }
+
+
+            //showlabel.Text = res.ToString();
+
+        }
+
+        private bool chkurl(string source)
+        {
+            return Android.Util.Patterns.WebUrl.Matcher(source).Matches();
         }
     }
 }
+     
 
